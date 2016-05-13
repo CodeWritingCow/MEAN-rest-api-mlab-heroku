ebb8a7747758aa70793b29ffc51418bbacda279d
Gary Pang <gary.pang@gmail.com>
2016-05-13 02:43:12 -0400

    Create EditContactCtrl
    Make Angular controller for editing contacts. Add methods to
    contactsService.

--- public/js/app.js
+++ public/js/app.js
@@ -16,6 +16,10 @@ angular.module('contactsApp', ['ngRoute'])
 				templateUrl: "contact-form.html"
 			})
 
+			.when("/contact/:contactId", {
+				controller: "EditContactController",
+				templateUrl: "contact.html"
+			})
 			.otherwise({
 				redirectTo: "/"
 			})
@@ -38,6 +42,36 @@ angular.module('contactsApp', ['ngRoute'])
 				alert("Error creating contact.");
 			});
 		}
+		this.getContact = function(contactId) {
+			var url = "/contacts/" + contactId;
+			return $http.get(url)
+			.then(function(response) {
+				return response;
+			}, function(response) {
+				alert("Error finding this contact.");
+			});
+		}
+		this.editContact = function(contact) {
+			var url = "/contacts/" + contact._id;
+			console.log(contact._id);
+			return $http.put(url, contact)
+			.then(function(response) {
+				return response;
+			}, function(response) {
+				alert("Error editing this contact.");
+				console.log(response);
+			});
+		}
+		this.deleteContact = function(contactId) {
+			var url = "/contacts/" + contactId;
+			return $http.delete(url)
+			.then(function(response) {
+				return response;
+			}, function(response) {
+				alert("Error deleting this contact.");
+				console.log(response);
+			});
+		}
 	})
 
 	.controller('ListController', function(contacts, $scope){
@@ -55,5 +89,33 @@ angular.module('contactsApp', ['ngRoute'])
 			}, function(response) {
 				alert(response);
 			});
+		}
+	})
+
+	.controller('EditContactController', function($scope, $routeParams, Contacts) {
+		Contacts.getContact($routeParams.contactId).then(function(doc) {
+			$scope.contact = doc.data;
+		}, function(response) {
+			alert(response);
+		});
+
+		$scope.toggleEdit = function() {
+			$scope.editMode = true;
+			$scope.contactFormUrl = "contact-form.html";
+		}
+
+		$scope.back = function() {
+			$scope.editMode = false;
+			$scope.contactFormUrl = "";
+		}
+
+		$scope.saveContact = function(contact) {
+			Contacts.editContact(contact);
+			$scope.editMode = false;
+			$scope.contactFormUrl = "";
+		}
+
+		$scope.deleteContact = function(contactId) {
+			Contacts.deleteContact(contactId);
 		}
 	});
